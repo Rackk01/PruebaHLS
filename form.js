58a4ba6a62d5344799bcf1b2b045e5d3133b3cbb
Facu_Diaz <108439711+Rackk01@users.noreply.github.com>
2023-04-21 12:44:31 -0300

    app fomulario terminada

--- form.js
+++ form.js
@@ -6,22 +6,16 @@ formulario.addEventListener("submit", function (event) {
   let datos = new FormData(formulario);
 
   if(datos.get("usuario").trim() === '' || datos.get("pass").trim() === ''){
-    Swal.fire({
-      icon: 'error',
-      title: 'Oops... Ha ocurrido un error',
-      text: 'Revisa que los datos ingresados sean correctos!',
-      footer: '<a href="">Why do I have this issue?</a>'
-    })
+    MostrarMensaje('error','Oops... Ha ocurrido un error','Ten cuidado con dejar campos vacíos!!');
     return;
-  }else{
-    Swal.fire(
-      'Excelente!',
-      'Buen trabajo!',
-      'success'
-    )
-  }
 
+  }
+  else{
+    // MostrarMensaje('success','GENIO','Ingresaste correctamente los datos!!');
+  }
+  
   loginUser(datos.get("usuario"), datos.get("pass"));
+
 });
 
 const loginUser = async (email, pass) => {
@@ -29,7 +23,8 @@ const loginUser = async (email, pass) => {
 
   formData.append("email", email);
   formData.append("pass", pass);
-
+  formData.append("funcion", "loginUser");
+  
   try {
     const res = await fetch("http://localhost/PruebaHLS/formulario.php", {
       method: "POST",
@@ -37,35 +32,95 @@ const loginUser = async (email, pass) => {
     });
 
     const data = await res.json();
-    // console.log(typeof(data));
     console.log(data);
+
+    const alerta = document.getElementById("alerta");
 
     if (data.success) {
 
-        Swal.fire({
-          icon: 'success',
-          title: 'Bien hecho',
-          text: data.goodmessage,
-          footer: '<a href="">Why do I have this issue?</a>'
-        })
+      // Mensaje estatico SUCCESS por registro exitoso del usuario en la DB
+      alerta.textContent = data.goodmessage;
+      alerta.classList.remove("alert-danger");
+      alerta.classList.add("alert-success");
 
-    }else{
+      // Mensaje pop-up SUCCESS por registro exitoso del usuario en la DB
       Swal.fire({
+  
+        icon: 'success',
+        title: data.goodmessage,
+        text: 'Has iniciado correctamente',
+      })
+    } else {
+
+      // Mensaje de ERROR estatico por usuario ya registrado en la DB
+      alerta.textContent = data.message;
+      alerta.classList.remove("alert-success");
+      alerta.classList.add("alert-danger");
+
+      Swal.fire({
+
+        // Mensaje pop-up ERROR por usuario ya registrado en la DB
+  
         icon: 'error',
         title: data.message,
-        text: 'Revisa que los datos ingresados sean correctos!',
-        footer: '<a href="">Why do I have this issue?</a>'
+        text: 'Intente con otro usuario',
       })
+
     }
+
+    // Mostrar la alerta
+    alerta.classList.remove("d-none");
+
+    // Ocultar la alerta después de 7 segundos
+    setTimeout(() => {
+      alerta.classList.add("d-none");
+    }, 7000);
+
   } catch (error) {
     console.log(error);
   }
 };
 
 
+function MostrarMensaje(icono, titulo, texto){
+  const alerta = document.getElementById("alertaInfo");
 
-// saludo('MAÑANAS');
+  Swal.fire({      
+    // Mensaje pop-up de error por inputs vacios (mensaje manual)
+    icon: icono,
+    title: titulo,
+    text: texto,
+    
+  });
 
-// function saludo(parteDelDia){
-//     console.log("BUENAS " + parteDelDia);
-// }
+  // 1 Mostrar el alert bootstrap con las clases y mensajes correspondientes
+
+  if (icono == "error"){
+
+    console.log("es error")
+
+    alerta.textContent = texto;
+    alerta.classList.remove("alert-success");
+    alerta.classList.remove("alert-info");
+    alerta.classList.add("alert-danger");
+
+    document.getElementById('idParrafo').innerHTML = texto;
+  } else{
+
+    alerta.textContent = texto;
+    alerta.classList.remove("alert-danger");
+    alerta.classList.remove("alert-info");
+    alerta.classList.add("alert-success");
+  }
+
+  //Mostrar la alerta en pantalla
+  alerta.classList.remove("d-none");
+
+    //Ocultar la alerta después de 7 segundos
+    setTimeout(() => {
+      alerta.classList.add("d-none");
+    }, 7000);
+
+
+}
+
--- formulario.php
+++ formulario.php
@@ -1,81 +1,91 @@
 <?php
+session_start();
+//Conexion a la base de datos.
 
+$funcion = $_POST["funcion"];
+
+if (!isset($funcion)) {
+    echo ('Error: La función no está especificada');
+    die;
+    return;
+}
 //CONEXION A LA BASE DE DATOS.
-
 $host = "localhost";
 $port = "5435";
 $dbname = "db_prueba";
 $user = "postgres";
 $password = "113355";
 
-$conex = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
-if (!$conex) {
+// Validar si la función no está vacía
+if (isset($funcion) && !empty($funcion)) {
 
-    die('Error de conexión: ' . pg_last_error());
-} else {
-    // echo 'Conexión Establecida';
-}
-
-
-$usuario = $_POST["email"];
-$pass = $_POST["pass"];
-
-//VALIDACION DE DATOS DE LA BASE DE DATOS
-
-if (trim($usuario) === "" && trim($pass) === "") {
-    // Inicio de sesión fallido
-    echo $respuesta = array("success" => false, "message" => "Inicio de sesión fallido. Por favor, verifique su usuario y contraseña.");
-    die;
-    return;
-}
-
-// Revisar si el correo existe en la base de datos
-$query = "SELECT correo FROM usuario WHERE correo = '$usuario'";
-$resultado = pg_query($conex, $query);
-
-if (!$resultado) {
-    echo("Error en la consulta: " . pg_last_error());
-    return;
-
-} else {
-    $num_rows = pg_num_rows($resultado);
-    if ($num_rows > 0) {
-        // El correo ya existe en la base de datos
-        $resultado = array("success" => false, "message" => "El correo ya está registrado.");
-        echo json_encode($resultado);
-        return;
+    $conex = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
+    if (!$conex) {
+        die('Error de conexión: ' . pg_last_error());
     } else {
+        // echo 'Conexión Establecida';
+    }
+} else {
+    // Manejar caso cuando la función está vacía
+    die('Error: La función no está especificada');
+}
 
-        // Insertar el nuevo usuario en la base de datos
-        $sql = "INSERT INTO usuario (correo, password) VALUES ('$usuario', '$pass') returning correo";
-        $resultado = pg_query($conex, $sql);
 
-        if ($resultado) {
-            // Inicio de sesión exitoso
-            $resultado = array("success" => true, "goodmessage" => "Inicio de sesión exitoso. Bienvenido, $usuario.");
+// Switch con el inicio de sesion y sus respectivos mensajes.
+
+switch ($funcion) {
+
+    case "loginUser":
+        // Código a ejecutar si $variable es igual a valor1
+        $usuario = $_POST["email"];
+        $pass = $_POST["pass"];
+
+
+        //VALIDACION DE DATOS DE LA BASE DE DATOS
+
+        if (trim($usuario) === "" && trim($pass) === "") {
+            // Inicio de sesión fallido
+            echo $respuesta = array("success" => false, "badmessage" => "Inicio de sesión fallido. Por favor, verifique su usuario y contraseña.");
+            echo json_encode($respuesta);
+            die;
+            return;
+        }
+
+        // REVISAR SI EL CORREO ESTA REGISTRADO EN LA DB.
+
+
+        $query = "SELECT * FROM usuario WHERE correo = '$usuario' AND password = '$pass'";
+        $resultado = pg_query($conex, $query);
+        $num_rows = pg_num_rows($resultado);
+    
+        if ($num_rows > 0) {
+            // El correo ya existe en la base de datos
+            $resultado = array("success" => false, "message" => "Ese usuario ya está registrado.");
             echo json_encode($resultado);
+            return;
         } else {
 
-            // Error en la conexión
-            $resultado = array("success" => false, "message" => "Hubo algún error en la conexión.");
-            echo json_encode($resultado);
+            //  Si no existe en la DB, insertar el nuevo usuario en la base de datos.
+            $sql = "INSERT INTO usuario (correo, password) VALUES ('$usuario', '$pass') returning correo";
+            $resultado = pg_query($conex, $sql);
+
+            if ($resultado) {
+                // Inicio de sesión exitoso
+                $resultado = array("success" => true, "goodmessage" => "Inicio de sesión exitoso. Bienvenido, $usuario.");
+                echo json_encode($resultado);
+            } else {
+                // Error en la conexión
+                $resultado = array("success" => false, "messageErrConex" => "Hubo algún error en la conexión.");
+                echo json_encode($resultado);
+            }
         }
-    }
+        break;
+
+    case "":
+        // Código a ejecutar si $variable es igual a valor3
+        break;
+        // ... más casos
+    default:
+        // Código a ejecutar si ninguno de los casos anteriores se cumple
+        break;
 }
-
-
-
-// $query = "SELECT correo, password FROM usuario";
-// $result = pg_query($conex, $query);
-
-// if (!$result) {
-//     die("Error en la consulta: " . pg_last_error());
-// } else {
-//     while ($row = pg_fetch_assoc($result)) {
-//         echo "Correo: " . $row['correo'] . "<br>";
-//         echo "Contraseña: " . $row['contraseña'] . "<br>";
-//     }
-// }
-
-
-
